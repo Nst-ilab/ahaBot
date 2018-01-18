cdcf2c6e303f8c6a2ea645cfa0f781973e348e38
EC2 Default User <ec2-user@ip-10-0-0-12.ap-southeast-1.compute.internal>
2018-01-18 07:14:46 +0000

    aha_Bot実装

--- ahaBot/lambda_function.py
+++ ahaBot/lambda_function.py
@@ -1,3 +1,41 @@
+import random
+
 def lambda_handler(event, context):
+    
     # TODO implement
-    return ''
+    language = event["analysedMessage"]["language"]
+    sentiment_score = event["analysedMessage"]["documentSentiment"]["score"]
+    
+    #分岐判断　ニュートラル、
+    if  -1.0 <= sentiment_score <= -0.33:
+        msg_sentiment = "negative"
+    elif -0.33 < sentiment_score < 0.33:
+        msg_sentiment = "neutral"
+    else: 
+        msg_sentiment = "positve"
+
+    #辞書定義。あとでconstに移す
+    aha_dic = {
+        "あぁ...":{"sentiment":"negative","language":"ja"},
+        "たいへんだね":{"sentiment":"negative","language":"ja"},
+        "へぇ":{"sentiment":"neutral","language":"ja"},
+        "たしかに!":{"sentiment":"positve","language":"ja"}
+    }
+    
+    response = []
+    #検索
+    for word,word_attribute in aha_dic.items():
+        if word_attribute["sentiment"] == msg_sentiment:
+            response.append(word)
+    
+    return random.choice(response)
+
+    #if sentiment_score in range(-1.0,-0.33):
+    #    sentiment = "negative"
+    #elif sentiment_score in range(-0.33,0.33):
+    #    sentiment = "neutral"
+    #else: 
+    #    sentiment = "positve"
+ #   aha_list = []
+ #   aha_list = ['']
+ #   return sentiment
